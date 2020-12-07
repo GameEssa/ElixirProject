c0625fda1cd8d5587025e45295252cf9a50e2ec4
GameEssa <1291053023@qq.com>
2020-12-07 19:50:09 +0800

    Server

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,35 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KVServer.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def accept( port ) do
+    {:ok, socket} =
+      :gen_tcp.listen( port, [:binary, packet: :line, active: false, reuseaddr: true] )
+    Logger.info( "Listen Port #{port}" )
+    server_loop( socket )
   end
+
+
+  defp server_loop( socket ) do
+    {:ok, client} = :gen_tcp.accept( socket )
+    client_loop(client)
+    server_loop( socket )
+  end
+
+  defp client_loop( client ) do
+    client
+    |> read_line()
+    |> write_line(client)
+
+    client_loop(client)
+  end
+
+  defp read_line( socket ) do
+    {:ok, data} = :gen_tcp.recv( socket, 0 )
+    data
+  end
+
+  defp write_line( line, socket ) do
+    :gen_tcp.send( socket, line )
+  end
+
 end
